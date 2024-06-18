77f3aabb6bdeb9333f8794427e88581b1fe73f14
daniescobarx <daniela.mollo@sptech.school>
2024-06-18 15:19:31 -0300

    add microservice-aluno

---
+++ microservice-aluno/src/main/java/com/microservice/aluno/MicroserviceAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAlunoApplication.class, args);
+	}
+
+}
---
+++ microservice-aluno/src/test/java/com/microservice/aluno/MicroserviceAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
