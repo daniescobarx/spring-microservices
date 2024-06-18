e7ed723cea633b36e9405f66d0c25a07999560a2
daniescobarx <daniela.mollo@sptech.school>
2024-06-18 19:46:25 -0300

    add estudanteController

---
+++ microservice-aluno/src/main/java/com/microservice/aluno/controller/EstudanteController.java
@@ -0,0 +1,32 @@
+package com.microservice.aluno.controller;
+
+import com.microservice.aluno.entities.Estudante;
+import com.microservice.aluno.service.IEstudanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/estudante")
+public class EstudanteController {
+
+    @Autowired
+    private IEstudanteService estudanteService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveEstudante(@RequestBody Estudante estudante){
+        estudanteService.save(estudante);
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllEstudante(){
+        return ResponseEntity.ok(estudanteService.findAll());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(estudanteService.findByid(id));
+    }
+}
