ed4fb252ee93b8947880280acee699e157ce5678
daniescobarx <daniela.mollo@sptech.school>
2024-06-18 17:39:28 -0300

    add microservice-curso

---
+++ microservice-curso/src/test/java/com/microservice/curso/MicroserviceCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
