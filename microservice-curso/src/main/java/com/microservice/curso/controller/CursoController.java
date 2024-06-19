92cff6053c3faa6f48ce21c7c439781e6d0a6d22
daniescobarx <daniela.mollo@sptech.school>
2024-06-18 21:03:20 -0300

    add CursoController - microserviceCurso

---
+++ microservice-curso/src/main/java/com/microservice/curso/controller/CursoController.java
@@ -0,0 +1,33 @@
+package com.microservice.curso.controller;
+
+import com.microservice.curso.entities.Curso;
+import com.microservice.curso.persistence.ICursoRepository;
+import com.microservice.curso.service.ICursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/curso")
+public class CursoController {
+
+    @Autowired
+    private ICursoService cursoService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveEstudante(@RequestBody Curso curso){
+        cursoService.save(curso);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllEstudantes(){
+        return ResponseEntity.ok(cursoService.findAll());
+    }
+
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(cursoService.findById(id));
+    }
+
+}
