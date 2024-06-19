f4a48cbbac62616f5ccba4847202c906afd7339a
daniescobarx <daniela.mollo@sptech.school>
2024-06-19 00:01:24 -0300

    feat: implementação de cursoServiceImpl, EstudanteClient

--- microservice-curso/src/main/java/com/microservice/curso/service/CursoServiceImpl.java
+++ microservice-curso/src/main/java/com/microservice/curso/service/CursoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.microservice.curso.service;
 
+import com.microservice.curso.client.EstudanteClient;
+import com.microservice.curso.dto.EstudanteDTO;
 import com.microservice.curso.entities.Curso;
+import com.microservice.curso.http.response.EstudanteByCursoResponse;
 import com.microservice.curso.persistence.ICursoRepository;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +14,14 @@ import java.util.List;
 @Service
 public class CursoServiceImpl implements ICursoService {
 
+    private final ICursoRepository cursoRepository;
+    private final EstudanteClient estudanteClient;
+
     @Autowired
-    private ICursoRepository cursoRepository;
+    public CursoServiceImpl(ICursoRepository cursoRepository, EstudanteClient estudanteClient) {
+        this.cursoRepository = cursoRepository;
+        this.estudanteClient = estudanteClient;
+    }
 
     @Override
     public List<Curso> findAll() {
@@ -26,5 +36,20 @@ public class CursoServiceImpl implements ICursoService {
     @Override
     public void save(Curso curso) {
         cursoRepository.save(curso);
+    }
+
+    @Override
+    public EstudanteByCursoResponse findEstudantesByIdCurso(Long idCurso) {
+        //consulta do curso
+        Curso curso = cursoRepository.findById(idCurso).orElse(new Curso());
+
+        //estudantes list
+        List<EstudanteDTO> estudanteDTOList = estudanteClient.findAllEstudanteByCurso(idCurso);
+
+        return EstudanteByCursoResponse.builder()
+                .nomeCurso(curso.getNome())
+                .professor(curso.getProfessor())
+                .estudanteDTOList(estudanteDTOList)
+                .build();
     }
 }
