02b79ec2f675d041c6384b283de541cefbbd0bf3
daniescobarx <daniela.mollo@sptech.school>
2024-06-19 10:08:14 -0300

    chore: add import.sql microservice-Aluno

---
+++ microservice-aluno/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO courses (name, teacher) VALUES ('Matematica', 'Professora Ana');
+INSERT INTO courses (name, teacher) VALUES ('Física', 'Professora Bianca');
+INSERT INTO courses (name, teacher) VALUES ('Historia', 'Professora Camila');
+INSERT INTO courses (name, teacher) VALUES ('Literatura', 'Professora Daniela');
+INSERT INTO courses (name, teacher) VALUES ('Biología', 'Professora Emily');
