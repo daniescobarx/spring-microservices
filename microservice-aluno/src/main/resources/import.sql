361f94f2a295fc48698bd76d057d7443c85d427b
daniescobarx <daniela.mollo@sptech.school>
2024-06-24 22:18:44 -0300

    att

--- microservice-aluno/src/main/resources/import.sql
+++ microservice-aluno/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO alunos (nome, sobrenome, email, cursoId) VALUES ('Joao', 'Souza', 'joao.souza@example.com', 1);
-INSERT INTO alunos (nome, sobrenome, email, cursoId) VALUES ('Maria', 'Mendes', 'maria.mendes@example.com', 2);
-INSERT INTO alunos (nome, sobrenome, email, cursoId) VALUES ('Carlos', 'Fontes', 'carlos.fontes@example.com', 3);
-INSERT INTO alunos (nome, sobrenome, email, cursoId) VALUES ('Ana', 'Silva', 'ana.silva@example.com', 1);
-INSERT INTO alunos (nome, sobrenome, email, cursoId) VALUES ('Pedro', 'Escobar', 'pedro.escobar@example.com', 3);
+INSERT INTO alunos (nome, sobrenome, email, curso_id) VALUES ('Joao', 'Souza', 'joao.souza@example.com', 1);
+INSERT INTO alunos (nome, sobrenome, email, curso_id) VALUES ('Maria', 'Mendes', 'maria.mendes@example.com', 2);
+INSERT INTO alunos (nome, sobrenome, email, curso_id) VALUES ('Carlos', 'Fontes', 'carlos.fontes@example.com', 3);
+INSERT INTO alunos (nome, sobrenome, email, curso_id) VALUES ('Ana', 'Silva', 'ana.silva@example.com', 1);
+INSERT INTO alunos (nome, sobrenome, email, curso_id) VALUES ('Pedro', 'Escobar', 'pedro.escobar@example.com', 3);
